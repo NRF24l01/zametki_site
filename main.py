d7f62481174c640e283195d109555432e6dc5bcd
snevr <s.nevrovsky@ya.ru>
2023-05-20 22:38:55 +0300

    create database and file with sqlite functions.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from flask import Flask, session, redirect,  render_template,  abort, request
+import
+
+app = Flask(__name__)
+app.secret_key = 'Stas_krutoy'
+
+# Страница со списком задач
+@app.route('/')
+def index():
+    done_count = len([task for task in tasks if task['is_done']])
+    count = len(tasks)
+    return render_template("tasks.html", tasks=tasks, done_count=done_count, count=count)
