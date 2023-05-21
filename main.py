e5bae644dd63d5efe046a39a678e98bb4273be35
snevr <s.nevrovsky@ya.ru>
2023-05-21 21:59:04 +0300

    Add registration sistem

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
-from flask import Flask, session, redirect,  render_template,  abort, request
+from flask import Flask, session, redirect, render_template, abort, request
 from sqlite_defs import *
+from config import *
 
 app = Flask(__name__)
 app.secret_key = 'Stas_krutoy'
 
+
 # Страница со списком задач
+
 @app.route('/')
 def index():
-    done_count = len([task for task in tasks if task['is_done']])
-    count = len(tasks)
-    return render_template("tasks.html", tasks=tasks, done_count=done_count, count=count)
+    return redirect('/login')
+
+
+@app.route('/tasks/add', methods=['post'])
+def add_task():
+    task_name = request.form.get('task_name')
+    task_description = request.form.get('task_description', '')
+    user_name = session.get('name', False)
+    print(task_name, task_description)
+
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/login', methods=['post'])
+def login_post():
+    #print(request.form)
+    user_name = request.form.get('username')
+    password = request.form.get('password')
+    suc = login(user_name, password)
+    if suc:
+        session['name'] = user_name
+        session['pass'] = password
+        return redirect('/tasks')
+    else:
+        session.clear()
+        return render_template('login.html', fleksim='ИМЯ ПОЛЬЗАВАТЕЛЯ ИЛИ ПАРОЛЬ ИНВАЛИДЫ')
+
+
+@app.route('/login', methods=['get'])
+def login_get():
+    session['auth'] = True
+    return render_template('login.html', fleksim='')
+
+
+app.run(debug=True, port='8000', host='0.0.0.0')
--- sqlite_defs.py
+++ sqlite_defs.py
@@ -1,1 +1,81 @@
 import sqlite3
+from config import db_name
+
+def get_task_list():
+    try:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        who_like = cursor.execute("SELECT COUNT(*) FROM zametki").fetchall()[0][0]
+
+        conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error26: ", error)
+
+    finally:
+        if (conn):
+            conn.close()
+
+def new_zametka(user_name:str, title:str, text:str):
+    try:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        cursor.execute(f"INSERT INTO 'zametki' (user_name, title, text) VALUES ()")
+
+        conn.commit()
+    except sqlite3.Error as error:
+        print("Error sql8: ", error)
+
+    finally:
+        if conn:
+            conn.close()
+
+def login(user_name:str, password:str) -> bool:
+    try:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        dt_pass = cursor.execute(f"SELECT pass FROM user WHERE name == '{user_name}'").fetchall()
+
+        conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error26: ", error)
+
+    finally:
+        if (conn):
+            conn.close()
+
+    try:
+        bob = dt_pass[0][0]
+    except:
+        bob = "None"
+    #print(user_name, password)
+    if bob == "None":
+        #print("bobiki")
+        new_user(user_name, password)
+        return True
+    else:
+        if password==dt_pass:
+            return True
+        else:
+            return False
+
+def new_user(user_name:str, password:str):
+    #print(user_name, password)
+    try:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        cursor.execute(f"INSERT INTO 'user' (name, pass) VALUES ('{user_name}','{password}')")
+
+        conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error26: ", error)
+
+    finally:
+        if (conn):
+            conn.close()
